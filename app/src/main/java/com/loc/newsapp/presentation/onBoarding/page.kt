cc9422d4ebb605cc619b3d87b8829c51db020dfd
Remon9 <96958659+Remon9@users.noreply.github.com>
2025-03-31 18:29:04 +0200

    first push

---
+++ app/src/main/java/com/loc/newsapp/presentation/onBoarding/page.kt
@@ -0,0 +1,11 @@
+package com.loc.newsapp.presentation.onBoarding
+
+import androidx.annotation.DrawableRes
+
+data class page(
+    val title:String,
+    val descripition:String,
+    @DrawableRes val image:Int
+)
+
+
